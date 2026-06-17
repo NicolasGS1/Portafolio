dfdc1f043cd822e5748dd58a30c2d28a1dfb0b23
Nicolas G <nicoavenue6@gmail.com>
2026-06-17 15:39:53 -0500

    new update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
-import { Component, AfterViewInit, OnDestroy } from '@angular/core';
-import { CommonModule } from '@angular/common'
-
-declare var VANTA: any;
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -10,35 +8,9 @@ declare var VANTA: any;
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-
-export class AppComponent implements AfterViewInit, OnDestroy{
-  private vantaEffect: any;
-  ngAfterViewInit(): void {setTimeout(() => {
-      if (typeof VANTA !== 'undefined') {
-        this.vantaEffect = VANTA.GLOBE({
-          el: document.getElementById('VANTA'),
-          mouseControls: true,
-          touchControls: true,
-          minHeight: 200.00,
-          minWidth: 200.00,
-          scale: 1.00,
-          scaleMobile: 1.00,
-          color: 0xffffff,
-          backgroundColor: 0x000000
-        });
-      } else {
-        console.error('VANTA is not defined');
-      }
-    }, 100);
-  }
-
-  ngOnDestroy(): void {
-    if (this.vantaEffect) {
-      this.vantaEffect.destroy();
-    }
-  }
-
-  //Funcion para los proyectos
+export class AppComponent {
+  
+  // Arreglo de proyectos para la vista
   proyectos = [
     {
       titulo: 'Menu para restaurante',
@@ -64,8 +36,5 @@ export class AppComponent implements AfterViewInit, OnDestroy{
       img: 'https://i.ibb.co/Ngtp2HpF/logo.png',
       url: 'https://imgur.com/a/xY0c99s'
     }
-    // Agrega más aquí...
   ];
 }
-
-
